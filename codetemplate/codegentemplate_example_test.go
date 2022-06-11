ec717d036b3a3ab9aba6b59f7f63bb579aa9bbe4
Red Daly <reddaly@gmail.com>
2022-06-11 12:36:40 -0700

    add codegentemplate example

---
+++ codetemplate/codegentemplate_example_test.go
@@ -0,0 +1,58 @@
+package codetemplate_test
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/meta-programming/go-codegenutil"
+	"github.com/meta-programming/go-codegenutil/codetemplate"
+)
+
+func Example() {
+	template, err := codetemplate.Parse("mypkg.go", `// Package mypkg does neat things.
+{{header}}
+
+var result1 = {{.maxFn1}}(1, 2)
+var result2 = {{.maxFn2}}(1, 2)
+
+func main() {
+	fmt.Printf("%f and %f\n", result1, result2)
+	fmt.Printf("The people's choice: %f\n", {{.peoplesChoice}})
+}
+	`)
+	if err != nil {
+		fmt.Printf("error parsing template: %v", err)
+		return
+	}
+
+	filePackage := codegenutil.AssumedPackageName("abc.xyz/mypkg")
+
+	code := &strings.Builder{}
+
+	if err := template.Execute(codegenutil.NewFileImports(filePackage), code, map[string]*codegenutil.Symbol{
+		"maxFn1":        codegenutil.Sym("math", "Max"),
+		"maxFn2":        codegenutil.Sym("alternative/math", "Max"),
+		"peoplesChoice": filePackage.Symbol("result2"),
+	}); err != nil {
+		fmt.Printf("error executing template: %v", err)
+		return
+	}
+
+	fmt.Print(code.String())
+	// Output: // Package mypkg does neat things.
+	// package mypkg
+	//
+	// import (
+	// 	"math"
+	//
+	// 	math2 "alternative/math"
+	// )
+	//
+	// var result1 = math.Max(1, 2)
+	// var result2 = math2.Max(1, 2)
+	//
+	// func main() {
+	// 	fmt.Printf("%f and %f\n", result1, result2)
+	// 	fmt.Printf("The people's choice: %f\n", result2)
+	// }
+}
