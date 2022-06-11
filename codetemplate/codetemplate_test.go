c11ef930c86616d17e8948c8a8aea39e29fa5015
Red Daly <reddaly@gmail.com>
2022-06-11 16:43:26 -0700

    add test case

--- codetemplate/codetemplate_test.go
+++ codetemplate/codetemplate_test.go
@@ -27,11 +27,14 @@ func TestTemplate_Execute(t *testing.T) {
 // Doesn't do anything special, really.
 var myThing = {{.mysym}}
 var myThing2 = {{.mysym2}}
+
+const myNum {{.numType}} = 42
 `,
 			imports: codegenutil.NewFileImports(pkg1),
 			data: map[string]*codegenutil.Symbol{
-				"mysym":  codegenutil.AssumedPackageName("math").Symbol("Max"),
-				"mysym2": codegenutil.AssumedPackageName("alternative/math").Symbol("Max"),
+				"mysym":   codegenutil.AssumedPackageName("math").Symbol("Max"),
+				"mysym2":  codegenutil.AssumedPackageName("alternative/math").Symbol("Max"),
+				"numType": codegenutil.Sym("", "int64"),
 			},
 			want: `// docs
 package mypkg
@@ -45,6 +48,8 @@ import (
 // Doesn't do anything special, really.
 var myThing = math.Max
 var myThing2 = math2.Max
+
+const myNum int64 = 42
 `,
 		},
 	}
